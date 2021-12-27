997da3c82be781f4f3538529ed4c70a09b822e2b
L II <lii@Ls-MacBook-Pro.lan>
2021-12-27 01:27:45 -0600

    added mapbox onto page

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import tw from 'tailwind-styled-components'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 
+'pk.eyJ1IjoibGJhcm5lczg2IiwiYSI6ImNreG9ibm5oNjR6aGcydHFobXA1MjBoeWsifQ.kR4MGbtL0niOCl2-JqLjsg';
+
 
 export default function Home() {
+  useEffect(() => {
+  
+   const map = new mapboxgl.Map({
+     container: 'map',
+     style: 'mapbox://styles/mapbox/streets-v11',
+     center: [-99.29011, 39.39172],
+     zoom: 3,
+   });
+  
+  });
+
+  // const map = new mapboxgl.Map({
+  //   container: 'map',
+  //   style: 'mapbox://styles/mapbox/streets-v11'
+  //   });
+
+
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id='map'></Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300
+  flex flex-col bg-red-300 h-screen
 `
 
 const Map = tw.div`
