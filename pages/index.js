d71070448bae5ecaa8636a49e6c998d377e144f9
L II <lii@Ls-MacBook-Pro.lan>
2021-12-27 11:50:37 -0600

    updated search.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import tw from 'tailwind-styled-components'
 import Map from './components/Map'
+import Link from 'next/link'
 
 
 
@@ -14,18 +15,84 @@ export default function Home() {
   return (
     <Wrapper>
       <Map />
-      <ActionItems>Start</ActionItems>
+      <ActionItems>
+       <Header>
+         <UberLogo src='https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg' />
+        <Profile>
+          <Name>Lloyd B</Name>
+          <UserImage src='https://user-images.githubusercontent.com/70309736/147489719-3e0a2fe3-d202-44e2-9e8a-2e0921e90401.png' />
+        </Profile>
+       </Header>
+       <ActionButtons>
+         <Link href='/search' >
+          <ActionButton>
+            <ActionButtonImage src='https://i.ibb.co/cyvcpfF/uberx.png'/>
+            Ride
+          </ActionButton>
+          </Link>
+          <ActionButton>
+          <ActionButtonImage src='https://i.ibb.co/n776JLm/bike.png'/>
+            Wheels
+          </ActionButton>
+          <ActionButton>
+          <ActionButtonImage src='https://i.ibb.co/5RjchBg/uberschedule.png'/>
+            Reserve
+          </ActionButton>
+
+       </ActionButtons>
+        {/* ActionButton */}
+        {/* InputButton   */}
+        <InputButton>
+            Where to?
+
+        </InputButton>
+      </ActionItems>
     </Wrapper>
   )
 }
 
 const Wrapper = tw.div`
-  flex flex-col bg-red-300 h-screen
+  flex flex-col h-screen
 `
 
 
 
 const ActionItems = tw.div`
-flex-1
+flex-1 p-4
 
+`
+const Header = tw.div`
+flex justify-between items-center
+`
+
+const UberLogo = tw.img`
+h-28
+
+`
+
+const Profile = tw.div`
+flex items-center
+`
+
+const Name = tw.div`
+mr-4 w-20 text-sm
+`
+
+const UserImage = tw.img`
+h-12 w-12 rounded-full border border-gray-200 p-px
+`
+
+const ActionButtons = tw.div`
+flex
+`
+const ActionButton = tw.button`
+flex bg-gray-200 flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl
+`
+
+const ActionButtonImage = tw.img`
+h-3/5 
+`
+
+const InputButton =tw.div`
+h-20 bg-gray-200 text-2xl p-4 flex items-center mt-8
 `
